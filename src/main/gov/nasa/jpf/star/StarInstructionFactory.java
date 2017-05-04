6aaa6352efa1c1772195ea7c4a63045f3de68bae
Pham Hong Long <longph1989@gmail.com>
2017-05-04 21:23:34 +0800

    extend symbc instruction factory

--- src/main/gov/nasa/jpf/star/StarInstructionFactory.java
+++ src/main/gov/nasa/jpf/star/StarInstructionFactory.java
@@ -1,9 +1,14 @@
 package gov.nasa.jpf.star;
 
+import gov.nasa.jpf.Config;
 import gov.nasa.jpf.vm.Instruction;
 
-public class StarInstructionFactory extends gov.nasa.jpf.jvm.bytecode.InstructionFactory {
+public class StarInstructionFactory extends gov.nasa.jpf.symbc.SymbolicInstructionFactory {
 	
+	public StarInstructionFactory(Config conf) {
+		super(conf);
+	}
+
 	@Override
 	public Instruction aload(int localVarIndex) {
 		System.out.println("aload");
