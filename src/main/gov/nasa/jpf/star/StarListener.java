553c9e9458e88fbd0dd39aa9a364aef930bc6b6e
Pham Hong Long <longph1989@gmail.com>
2017-05-04 11:07:39 +0800

    init

---
+++ src/main/gov/nasa/jpf/star/StarListener.java
@@ -0,0 +1,8 @@
+package gov.nasa.jpf.star;
+
+import gov.nasa.jpf.PropertyListenerAdapter;
+import gov.nasa.jpf.report.PublisherExtension;
+
+public class StarListener extends PropertyListenerAdapter implements PublisherExtension {
+
+}
