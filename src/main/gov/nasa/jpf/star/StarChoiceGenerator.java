ef41728fae6dd4fe66467efc0a6aae91617df128
Pham Hong Long <longph1989@gmail.com>
2017-05-06 00:33:04 +0800

    choice generator

---
+++ src/main/gov/nasa/jpf/star/StarChoiceGenerator.java
@@ -0,0 +1,11 @@
+package gov.nasa.jpf.star;
+
+import gov.nasa.jpf.symbc.heap.HeapChoiceGenerator;
+
+public class StarChoiceGenerator extends HeapChoiceGenerator {
+
+	public StarChoiceGenerator(int size) {
+		super(size);
+	}
+
+}
