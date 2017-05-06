34fa911de7d698aeeee1a2291bf973a86c3dfdaa
Pham Hong Long <longph1989@gmail.com>
2017-05-06 20:48:07 +0800

    inductive predicate

---
+++ src/main/gov/nasa/jpf/inductive/InductivePred.java
@@ -0,0 +1,10 @@
+package gov.nasa.jpf.inductive;
+
+import gov.nasa.jpf.formula.Formula;
+
+public class InductivePred {
+	
+	// inductive predicate definition has form of a formula
+	private Formula formula;
+
+}
---
+++ src/main/gov/nasa/jpf/inductive/InductivePredMap.java
@@ -0,0 +1,11 @@
+package gov.nasa.jpf.inductive;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InductivePredMap {
+	
+	// contains all inductive predicate definition
+	private Map<String,InductivePred> inductivePredMap = new HashMap<String,InductivePred>();
+
+}
