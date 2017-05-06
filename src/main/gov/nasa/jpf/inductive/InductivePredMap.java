4fde766593c8b781ff1062cd986191a39e3417ee
Pham Hong Long <longph1989@gmail.com>
2017-05-06 20:48:55 +0800

    minor

--- src/main/gov/nasa/jpf/inductive/InductivePredMap.java
+++ src/main/gov/nasa/jpf/inductive/InductivePredMap.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class InductivePredMap {
 	
-	// contains all inductive predicate definition
+	// contains all inductive predicate definitions
 	private Map<String,InductivePred> inductivePredMap = new HashMap<String,InductivePred>();
 
 }
