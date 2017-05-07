aa6f16ecda5bf4962e7ad5a6bf0b581109df0717
Pham Hong Long <longph1989@gmail.com>
2017-05-07 20:49:53 +0800

    substitute empty

--- src/main/gov/nasa/jpf/formula/heap/Empty.java
+++ src/main/gov/nasa/jpf/formula/heap/Empty.java
@@ -1,5 +1,15 @@
 package gov.nasa.jpf.formula.heap;
 
+import java.util.Map;
+
+import gov.nasa.jpf.formula.Variable;
+
 public class Empty extends HeapTerm {
+	
+	@Override
+	public HeapTerm substitute(Variable[] params, Variable[] vars,
+			Map<String,String> existVarSubMap) {
+		return new Empty();
+	}
 
 }
