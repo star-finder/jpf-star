50f373c7882f0da739dbe1592b25a3b60ca72adf
Pham Hong Long <longph1989@gmail.com>
2017-05-07 20:45:53 +0800

    substitute heap formula

--- src/main/gov/nasa/jpf/formula/HeapFormula.java
+++ src/main/gov/nasa/jpf/formula/HeapFormula.java
@@ -10,8 +10,21 @@ public class HeapFormula {
 	
 	private HeapTerm[] heapTerms;
 	
+	public HeapFormula(HeapTerm[] heapTerms) {
+		this.heapTerms = heapTerms;
+	}
+	
 	public HeapFormula substitute(Variable[] params, Variable[] vars,
 			Map<String,String> existVarSubMap) {
+		int length = heapTerms.length;
+		HeapTerm[] newHeapTerms = new HeapTerm[length];
+		
+		for (int i = 0; i < length; i++) {
+			newHeapTerms[i] = heapTerms[i].substitute(params, vars, existVarSubMap);
+		}
+		
+		HeapFormula newHeapFormula = new HeapFormula(newHeapTerms);
+		
 		return this;
 	}
 
--- src/main/gov/nasa/jpf/formula/heap/HeapTerm.java
+++ src/main/gov/nasa/jpf/formula/heap/HeapTerm.java
@@ -1,5 +1,14 @@
 package gov.nasa.jpf.formula.heap;
 
+import java.util.Map;
+
+import gov.nasa.jpf.formula.Variable;
+
 public abstract class HeapTerm {
+	
+	public HeapTerm substitute(Variable[] params, Variable[] vars,
+			Map<String,String> existVarSubMap) {
+		return null;
+	}
 
 }
