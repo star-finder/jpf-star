db22c25bb5e8c07ac0a73208640dffb64bd51c80
Quoc-Sang Phan <dark2bright@gmail.com>
2017-09-30 22:52:41 -0700

    fix InductivePredParserTest

--- src/tests/star/inductive/InductivePredParserTest.java
+++ src/tests/star/inductive/InductivePredParserTest.java
@@ -14,7 +14,8 @@ public class InductivePredParserTest {
 
 	@Test
 	public void test1() {
-		String pred = "pred sll(x) == x = null || x->Node(next) * sll(next)";
+		String pred = "pred sll(x) == x = null || x::Node<next> * sll(next)";
+		String outPred = "pred sll(x) == x = null || x->Node(next) * sll(next)";
 		
 		ANTLRInputStream in = new ANTLRInputStream(pred);
         InductivePredLexer lexer = new InductivePredLexer(in);
@@ -22,14 +23,17 @@ public class InductivePredParserTest {
         InductivePredParser parser = new InductivePredParser(tokens);
         
         InductivePred[] ips = parser.preds().ips;
-        assertTrue(ips[0].toString().equals(pred));
+        assertTrue(ips[0].toString().equals(outPred));
 	}
 	
 	@Test
 	public void test2() {
-		String pred0 = "pred sll(root) == root = null || root->Node(next) * sll(next)";
-		String pred1 = "pred dll(root,prev) == root = null || root->Node2(prev,next) * dll(next,root)";
+		String pred0 = "pred sll(root) == root = null || root::Node<next> * sll(next)";
+		String pred1 = "pred dll(root,prev) == root = null || root::Node2<prev,next> * dll(next,root)";
 		String preds = pred0 + ";" + pred1;
+		
+		String outPred0 = "pred sll(root) == root = null || root->Node(next) * sll(next)";
+		String outPred1 = "pred dll(root,prev) == root = null || root->Node2(prev,next) * dll(next,root)";
 		
 		ANTLRInputStream in = new ANTLRInputStream(preds);
         InductivePredLexer lexer = new InductivePredLexer(in);
@@ -37,8 +41,8 @@ public class InductivePredParserTest {
         InductivePredParser parser = new InductivePredParser(tokens);
         
         InductivePred[] ips = parser.preds().ips;
-        assertTrue(ips[0].toString().equals(pred0));
-        assertTrue(ips[1].toString().equals(pred1));
+        assertTrue(ips[0].toString().equals(outPred0));
+        assertTrue(ips[1].toString().equals(outPred1));
 	}
 
 }
